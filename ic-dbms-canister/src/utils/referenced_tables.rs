f5ba3c6ef0cef48f68f3a9fd7a23261f3c2e7ec6
Christian Visintin <christian.visintin@veeso.dev>
2025-12-11 09:42:15 +0100

    fix: get_referencing_tables not working with self referencing tables

--- ic-dbms-canister/src/utils/referenced_tables.rs
+++ ic-dbms-canister/src/utils/referenced_tables.rs
@@ -20,14 +20,16 @@ pub fn get_referenced_tables(
     tables: &[(&'static str, &'static [ColumnDef])],
 ) -> Vec<(&'static str, Vec<&'static str>)> {
     let mut referenced_tables = vec![];
-    // iterate over tables different from target
-    for (table_name, columns) in tables.iter().filter(|(name, _)| *name != target) {
+    // iterate over tables
+    for (table_name, columns) in tables {
         let mut referenced_tables_columns = vec![];
-        // iterate over fks
-        for fk in columns.iter().filter_map(|col| col.foreign_key.as_ref()) {
-            if fk.foreign_table == target {
-                referenced_tables_columns.push(fk.local_column);
-            }
+        // iterate over fks with target table as foreign table
+        for fk in columns
+            .iter()
+            .filter_map(|col| col.foreign_key.as_ref())
+            .filter(|fk| fk.foreign_table == target)
+        {
+            referenced_tables_columns.push(fk.local_column);
         }
         if !referenced_tables_columns.is_empty() {
             referenced_tables.push((*table_name, referenced_tables_columns));
@@ -39,6 +41,8 @@ pub fn get_referenced_tables(
 
 #[cfg(test)]
 mod tests {
+
+    use ic_dbms_api::prelude::DataTypeKind;
 
     use super::*;
     use crate::prelude::TableSchema as _;
@@ -57,5 +61,35 @@ mod tests {
         assert_eq!(references[0].1, vec!["user"]);
         assert_eq!(references[1].0, "messages");
         assert_eq!(references[1].1, vec!["sender", "recipient"]);
+    }
+
+    const SELF_REFERENCING_TABLE: &[ColumnDef] = &[
+        ColumnDef {
+            name: "id",
+            data_type: DataTypeKind::Uint32,
+            nullable: false,
+            primary_key: true,
+            foreign_key: None,
+        },
+        ColumnDef {
+            name: "manager",
+            data_type: DataTypeKind::Uint32,
+            nullable: true,
+            primary_key: false,
+            foreign_key: Some(ic_dbms_api::prelude::ForeignKeyDef {
+                local_column: "manager",
+                foreign_table: "users",
+                foreign_column: "id",
+            }),
+        },
+    ];
+
+    #[test]
+    fn test_should_get_self_referenced_tables() {
+        let tables = &[("users", SELF_REFERENCING_TABLE)];
+        let references = get_referenced_tables("users", tables);
+        assert_eq!(references.len(), 1);
+        assert_eq!(references[0].0, "users");
+        assert_eq!(references[0].1, vec!["manager"]);
     }
 }
